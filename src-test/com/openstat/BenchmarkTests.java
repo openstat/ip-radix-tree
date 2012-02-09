a7f6c2c937523d34259758a63f33cfeb851a1ffe
Mikhail Yakshin <greycat@altlinux.org>
2012-02-09 17:33:37 +0400

    Added benchmarking tests

---
+++ src-test/com/openstat/BenchmarkTests.java
@@ -0,0 +1,67 @@
+package com.openstat;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class BenchmarkTests {
+    private static final long SLEEP_INTERVAL = 100;
+    public static int M = 10;
+
+    public static void gc() {
+        for (int i = 0; i < 3; i++) {
+            try {
+                System.gc();
+                Thread.currentThread().sleep(SLEEP_INTERVAL);
+                System.runFinalization();
+                Thread.currentThread().sleep(SLEEP_INTERVAL);
+            } catch (InterruptedException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Test public void benchmarkFromFileTest() throws IOException {
+        gc();
+        long m1 = Runtime.getRuntime().freeMemory();
+        IPv4RadixIntTree tr[] = new IPv4RadixIntTree[M];
+        for (int i = 0; i < M; i++)
+            tr[i] = IPv4RadixIntTree.loadFromLocalFile("test/ip-prefix-base.txt");
+        gc();
+        long m2 = Runtime.getRuntime().freeMemory();
+        System.out.format(
+                "%d * %d elements\n%d bytes\n%.2f bytes/element\n",
+                M, tr[0].size(),
+                m1 - m2,
+                (double) (m1 - m2) / (M * tr[0].size())
+        );
+    }
+
+    int N = 1000000;
+
+    @Test public void benchmarkRandomTest() throws IOException {
+        Random rnd = new Random(42);
+
+        gc();
+        long m1 = Runtime.getRuntime().freeMemory();
+
+        IPv4RadixIntTree tr = new IPv4RadixIntTree();
+
+        for (int i = 0; i < N; i++) {
+            int cidr = rnd.nextInt(33);
+            long netmask =  ((1L << (32 - cidr)) - 1L) ^ 0xffffffffL;
+            tr.put(rnd.nextLong() & 0xffffffff, netmask, rnd.nextInt());
+        }
+
+        gc();
+        long m2 = Runtime.getRuntime().freeMemory();
+
+        System.out.format(
+                "%d random elements\n%d bytes\n%.2f bytes/element\n",
+                tr.size(),
+                m1 - m2,
+                (double) (m1 - m2) / tr.size()
+        );
+    }
+}
